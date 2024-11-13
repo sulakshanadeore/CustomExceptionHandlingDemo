e81869d3afdd3e60e6076667580e7551164e5088
deoresulakshana <deoresulakshana@gmail.com>
2024-11-13 10:01:22 +0530

    Events and Multicast Delegates, Custoomer Deletion is unicast delegate with event and CustomerGreetingDelegate is muticast

--- CustomExceptionHandlingDemo/Customer.cs
+++ CustomExceptionHandlingDemo/Customer.cs
@@ -6,22 +6,73 @@ using System.Threading.Tasks;
 
 namespace CustomExceptionHandlingDemo
 {
+    internal delegate void RemoveCustomerDelegate(int custid);
+
+    internal delegate void CustomerGreetingsDelegate();
     internal class Customer
     {
         public int Custid { get; set; }
         public string CustName { get; set; }
     }
 
-
+    
 
     internal class CustomerUtility
     {
+        
+
         static List<Customer> list = new List<Customer>() 
         { new Customer {Custid=1,CustName="Ankit" },
             new Customer {Custid=2,CustName="Gauri" },
             new Customer {Custid=3,CustName="Sumit" }
 
             };
+
+
+
+
+        internal void GreetEveryCustomer()
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine("Good morning " +  item.CustName);
+            }
+        }
+
+
+        internal void PrintCustomers()
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.Custid + " " + item.CustName);
+            }
+
+        }
+
+
+        internal void DeleteCustomer(int custid)
+        {
+            Customer found = list.Find(c => c.Custid == custid);
+            if (found!=null)
+            {
+                list.Remove(found);
+
+
+                Console.WriteLine("After deleting the following customers exists:");
+                foreach (var item in list)
+                {
+                    Console.WriteLine(item.Custid + " " + item.CustName);
+                }
+            }
+            else
+            {
+                throw new CustomerNotFoundException("This is not a valid customer id....");
+            }
+
+        }
+
+
+
 
 
       internal  void FindCustomer(int custid)
--- CustomExceptionHandlingDemo/Program.cs
+++ CustomExceptionHandlingDemo/Program.cs
@@ -6,14 +6,61 @@ using System.Threading.Tasks;
 
 namespace CustomExceptionHandlingDemo
 {
+    
     internal class Program
     {
+        internal static event RemoveCustomerDelegate ev_CustomerDeletion;
+        internal static event CustomerGreetingsDelegate ev_Customers;
+
         static void Main(string[] args)
         {
             CustomerUtility u=new CustomerUtility();
             try
             {
-                u.FindCustomer(1);
+                Console.WriteLine("Menu 1 to find, 2 to  delete customer 3.Multicast delegate");
+                int ch=Convert.ToInt32(Console.ReadLine());
+                switch (ch)
+                {
+                    case 1:
+                        u.FindCustomer(1);
+                        break;
+                    case 2:
+                        RemoveCustomerDelegate del = new RemoveCustomerDelegate(u.DeleteCustomer);
+                        ev_CustomerDeletion += del;
+
+                        Console.WriteLine("Enter the customer id to delete");
+                        int custid=Convert.ToInt32(Console.ReadLine());
+                       ev_CustomerDeletion(custid);
+
+                        break;
+                    case 3:
+
+                        CustomerGreetingsDelegate[] delArray = new CustomerGreetingsDelegate[] {
+                        u.GreetEveryCustomer,
+                        u.PrintCustomers
+                        };
+                        //CustomerGreetingsDelegate del1=new CustomerGreetingsDelegate(u.GreetEveryCustomer);
+                        //CustomerGreetingsDelegate del2 = new CustomerGreetingsDelegate(u.PrintCustomers);
+                        //CustomerGreetingsDelegate del3=(CustomerGreetingsDelegate)MulticastDelegate.Combine(del1, del2);
+
+
+                        CustomerGreetingsDelegate del3 = (CustomerGreetingsDelegate)MulticastDelegate.Combine(delArray);
+                        //del3();
+                        ev_Customers += del3;
+                        ev_Customers();
+
+
+                        break;
+                    default:
+                        break;
+                }
+                
+                
+
+
+
+
+
             }
             catch (CustomerNotFoundException ex)
             {
